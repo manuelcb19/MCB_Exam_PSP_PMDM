c8fbb5a854d20b9541ce754a5cf3a534893d57c7
mcbtr <mcb.gitlab@gmail.com>
2023-11-30 18:21:49 +0100

    Primer commit del Examen, Se realiza la conexion con firebase. Ademas de eso se instala el firestone, cloud, autentificacion y el firestone, ya que mas adelante se necesitaran.

---
+++ lib/ExamenMCB.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ExamenMCB extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:examenmcb/ExamenMCB.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async{
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  ExamenMCB examenmcb= ExamenMCB();
+  runApp(examenmcb);
+
+
+}
