d0cee79ad3dde73889ca6c055fd39c4303156b79
mcbtr <mcb.gitlab@gmail.com>
2023-12-01 16:21:38 +0100

    PSP Se crea la clase fbPostId para usarla en un futuro para la creaccion de un post, se crea ya con un enlace a una imagen, nombre de usuario, uid del usuario, el contenido del post y el titulo de este. Ademas se pone un HomeView provisional

---
+++ lib/Home/HomeView.dart
@@ -0,0 +1,41 @@
+
+
+
+
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../CustomViews/CustomButton.dart';
+
+class HomeView extends StatefulWidget{
+
+  String uid=FirebaseAuth.instance.currentUser!.uid;
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _HomeViewState();
+  }
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("Examen"),
+        shadowColor: Colors.orangeAccent, // Color de sombra del AppBar
+        backgroundColor: Colors.orangeAccent,),
+      backgroundColor: Colors.amber[200],// Color de fondo del AppBar
+      body: Center(
+      ),
+      bottomNavigationBar: CustomButton(texto: (FirebaseAuth.instance.currentUser!.uid).toString(),),
+      floatingActionButton:FloatingActionButton(
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+      /**/
+    );
+  }
+
+}
