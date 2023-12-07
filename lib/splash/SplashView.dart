e78d1d8f7cbfd24feb72a2e0407d241b2bbd702d
mcbtr <mcb.gitlab@gmail.com>
2023-12-07 16:01:25 +0100

    PMDM se termina de ajustar el splas

--- lib/splash/SplashView.dart
+++ lib/splash/SplashView.dart
@@ -1,8 +1,10 @@
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:examenmcb/FirebaseObjects/FbUsuario.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
 
 
 class SplashView extends StatefulWidget {
@@ -21,7 +23,42 @@ class _SplashViewState extends State<SplashView>{
   void initState() {
     // TODO: implement initState
     super.initState();
+    checkSession();
   }
+
+  void checkSession() async{
+    await Future.delayed(Duration(seconds: 3));
+
+    if (FirebaseAuth.instance.currentUser != null) {
+
+      String uid = FirebaseAuth.instance.currentUser!.uid;
+
+      DocumentReference<FbUsuario> enlace = db.collection("Usuarios").doc(uid).withConverter(fromFirestore: FbUsuario.fromFirestore,
+        toFirestore: (FbUsuario usuario, _) => usuario.toFirestore(),);
+
+      FbUsuario usuario;
+
+      DocumentSnapshot<FbUsuario> docSnap = await enlace.get();
+      usuario = docSnap.data()!;
+
+      if (usuario != null) {
+
+          Navigator.of(context).popAndPushNamed("/homeview");
+        }
+
+      else{
+
+        Navigator.of(context).popAndPushNamed("/perfilview");
+      }
+    }
+
+    else{
+      Navigator.of(context).popAndPushNamed("/loginview");
+
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
